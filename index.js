1a664ae99520f4be79722d82f57365043a7b5cf4
Shane Gadsby <Shane.Gadsby@usq.edu.au>
2016-10-25 10:53:32 +1000

    pushing latest

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
 "use strict"
-
+ 
 var fs = require('fs'),
-	ics = fs.readFileSync(__dirname + '/template.ics').toString('utf8')
-
-
+    ics = fs.readFileSync(__dirname + '/template.ics').toString('utf8')
+ 
+ 
 module.exports = {
-	createICS: function createICS (options) {
-		var newICS = ics,
-			uuid = String((new Date()).getTime()),
-			whitelist = {
-				'organizerName':'',
-				'organizerEmail':'',
-				'attendeeName':'',
-				'attendeeEmail':'',
-				'body':'',
-				'subject':'',
-				'location':'',
-				'uuid': '123456789',
-				'status': 'CONFIRMED',
-				'start': '',
-				'end': '',
-				'method': 'REQUEST',
-				'attach': '',
-				'comment': '',
-				'sequence': '0',
-				'currentTime': '',
-			}
-
-
-		options.start = module.exports.createDateTime(options.start || new Date())
-		options.end = module.exports.createDateTime(options.end || new Date())
-		options.currentTime = module.exports.createDateTime(options.currentTime || new Date())
-
-
-		for (var i in whitelist) {
-			newICS = newICS.replace(new RegExp('---' + i + '---', 'g'), (options[i] || whitelist[i]))
-		}
-
-		return newICS
-	},
-
-	createDateTime: function createDateTime (date) {
-		return date.toISOString().replace(/-|:/g, '').slice(0, 13).concat('00Z')
-	},	
-
-	createNodemailerEvent: function createNodemailerAttachment (options) {
-		return {
-			method: options.method,
-			content: module.exports.createICS(options)
-		}
-	}
+    createICS: function createICS (options) {
+        var newICS = ics,
+            uuid = String((new Date()).getTime()),
+            whitelist = {
+                'organizerName':'',
+                'organizerEmail':'',
+                'attendeeName':'',
+                'attendeeEmail':'',
+                'body':'',
+                'subject':'',
+                'location':'',
+                'uuid': '123456789',
+                'status': 'CONFIRMED',
+                'start': '',
+                'end': '',
+                'method': 'REQUEST',
+                'attach': '',
+                'comment': '',
+                'sequence': '0',
+                'currentTime': '',
+            }
+ 
+ 
+        options.start = module.exports.createDateTime(options.start || new Date())
+        options.end = module.exports.createDateTime(options.end || new Date())
+        options.currentTime = module.exports.createDateTime(options.currentTime || new Date())
+ 
+ 
+        for (var i in whitelist) {
+            newICS = newICS.replace(new RegExp('---' + i + '---', 'g'), (options[i] || whitelist[i]))
+        }
+ 
+        return newICS
+    },
+ 
+    createDateTime: function createDateTime (date) {
+        var t = date.toISOString().replace(/-|:/g, '').slice(0, 13).concat('00Z')
+        console.log(t)
+        return t
+    },  
+ 
+    createNodemailerEvent: function createNodemailerAttachment (options) {
+        return {
+            method: options.method,
+            content: module.exports.createICS(options)
+        }
+    }
 }
 	
 /*Very basic example*/
